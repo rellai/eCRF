5d871a61cc24d79ac9183060ba5d86ffecd517ab
Anton Ivanov <rellai@yandex.ru>
2024-02-05 16:49:21 +0300

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/ru/rellai/ecrf/EcrfApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/ru/rellai/ecrf/EcrfApplicationTests.java
    
    Changes not staged for commit:
    	modified:   .gitignore

---
+++ src/main/java/ru/rellai/ecrf/EcrfApplication.java
@@ -0,0 +1,13 @@
+package ru.rellai.ecrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcrfApplication.class, args);
+	}
+
+}
