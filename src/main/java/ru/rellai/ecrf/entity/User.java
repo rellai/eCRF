89ad03b5e9bfc5d3f9c473fcc154a5f7e0533d5b
Anton Ivanov <rellai@yandex.ru>
2024-02-07 01:33:18 +0300

    create basic template

---
+++ src/main/java/ru/rellai/ecrf/entity/User.java
@@ -0,0 +1,30 @@
+package ru.rellai.ecrf.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+    private String authorities;
+}
